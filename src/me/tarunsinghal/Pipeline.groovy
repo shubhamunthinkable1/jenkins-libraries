72e74ac8767778d955e72192f41ce4b5673081d7
shubhamunthinkable1 <81431756+shubhamunthinkable1@users.noreply.github.com>
2021-09-16 18:09:16 +0530

    Update Pipeline.groovy

--- src/me/tarunsinghal/Pipeline.groovy
+++ src/me/tarunsinghal/Pipeline.groovy
@@ -49,7 +49,7 @@ def helmDeploy(Map args) {
         // reimplement --wait once it works reliable
         sh "helm upgrade --install ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag} --namespace=${namespace}"
 	    if (namespace == "default"){
-		    sh "helm upgrade --install ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag} --set imagePullSecrets[0].name=$(creds) --namespace=${namespace} --kubeconfig=/var/lib/jenkins/.kube/confignew"
+		  sh "helm upgrade --install ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag} --set imagePullSecrets[0].name=$(creds) --namespace=${namespace} --kubeconfig=/var/lib/jenkins/.kube/confignew"
 	    }
 	 
 
